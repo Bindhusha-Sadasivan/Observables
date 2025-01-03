c463fe0a7c7472556bd29ac7ecb7f035f962da81
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2025-01-03 05:41:44 +0530

    173 - Getting Closer to the Core of Observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,20 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  private firstObsSubscripition!: Subscription;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    this.firstObsSubscripition = interval(1000).subscribe( count => console.log(count));
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.firstObsSubscripition.unsubscribe();
+  }
 }
